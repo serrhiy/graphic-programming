a8a7dbecb0620b9fd87d2d6d1b549de39b30e665
Lytvynenko Serhiy <serrheylitvinenko@gmail.com>
2024-11-05 06:29:07 +0200

    add example with using ebo

--- main.cpp
+++ main.cpp
@@ -64,9 +64,15 @@ unsigned int createShaderProgram(const char* vertexSource, const char* fragmentS
 }
 
 const float points[] {
-  -0.5f, -0.5f, 0.f,
-   0.5f, -0.5f, 0.f,
-   0.0f,  0.5f, 0.f,
+   0.5f,  0.5f, 0.0f,
+   0.5f, -0.5f, 0.0f,
+  -0.5f, -0.5f, 0.0f,
+  -0.5f,  0.5f, 0.0f
+};
+
+const unsigned int indices[] {
+  0, 1, 3,
+  1, 2, 3
 };
 
 int main(const int argc, const char* argv[]) {
@@ -87,24 +93,29 @@ int main(const int argc, const char* argv[]) {
 
   const unsigned int shaderProgram = createShaderProgram(vertexShaderSource, fragmentShaderSource);
   
-  unsigned int VBO = 0, VAO = 0;
+  unsigned int VBO = 0, VAO = 0, EBO = 0;
   glGenVertexArrays(1, &VAO);
   glBindVertexArray(VAO);
   glGenBuffers(1, &VBO);
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof points, points, GL_STATIC_DRAW);
+  glGenBuffers(1, &EBO);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof indices, indices, GL_STATIC_DRAW);
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
   glEnableVertexAttribArray(0);
   glBindVertexArray(0);
   glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
+  glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
   while (!glfwWindowShouldClose(window)) {
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
     glUseProgram(shaderProgram);
     glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
     glfwSwapBuffers(window);
     glfwPollEvents();
