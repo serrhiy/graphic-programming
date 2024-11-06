85ede7a3be26e121eb2f318a5c34604f4c892bda
Lytvynenko Serhiy <serrheylitvinenko@gmail.com>
2024-11-06 02:43:34 +0200

    add example with color sendind to fragment shader

--- main.cpp
+++ main.cpp
@@ -19,20 +19,18 @@ void sizeChanging(GLFWwindow* window, int width, int height) {
 
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 position;"
+"layout (location = 1) in vec3 color;"
+"out vec4 vertexColor;"
 "void main() {"
 "  gl_Position = vec4(position, 1);"
+"  vertexColor = vec4(color, 1);"
 "}";
 
-const char* fragmentShaderOrange = "#version 330 core\n"
+const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 color;"
+"in vec4 vertexColor;"
 "void main() {"
-"  color = vec4(1.0f, 0.5f, 0.2f, 1.0f);"
-"}";
-
-const char* fragmentShaderYellow = "#version 330 core\n"
-"out vec4 color;"
-"void main() {"
-"  color = vec4(1.0f, 1.0f, 0.0f, 1.0f);"
+"  color = vertexColor;"
 "}";
 
 unsigned int createShaderProgram(const char* vertexSource, const char* fragmentSource) {
@@ -70,13 +68,9 @@ unsigned int createShaderProgram(const char* vertexSource, const char* fragmentS
 }
 
 const float points[] {
-  -0.5f, -0.5f, 0.0f,
-   0.0f, -0.5f, 0.0f,
-   0.0f,  0.5f, 0.0f,
-
-   0.0f,  0.5f, 0.0f,
-   0.5f, -0.5f, 0.0f,
-   0.0f, -0.5f, 0.0f,
+  -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+   0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+   0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
 };
 
 int main(const int argc, const char* argv[]) {
@@ -95,24 +89,20 @@ int main(const int argc, const char* argv[]) {
     return 1;
   }
 
-  const unsigned int shaderProgramOrange = createShaderProgram(vertexShaderSource, fragmentShaderOrange);
-  const unsigned int shaderProgramYellow = createShaderProgram(vertexShaderSource, fragmentShaderYellow);
+  const unsigned int shaderProgram = createShaderProgram(vertexShaderSource, fragmentShaderSource);
   
-  unsigned int VBOs[]{ 0, 0 }, VAOs[]{ 0, 0 };
-  glGenVertexArrays(2, VAOs);
-  glGenBuffers(2, VBOs);
+  unsigned int VBO{ 0 }, VAO{ 0 };
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
 
-  glBindVertexArray(VAOs[0]);
-  glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(points) / 2, points, GL_STATIC_DRAW);
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+  glBindVertexArray(VAO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof points, points, GL_STATIC_DRAW);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), nullptr);
   glEnableVertexAttribArray(0);
-  
-  glBindVertexArray(VAOs[1]);
-  glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(points) / 2, points + 9, GL_STATIC_DRAW);
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
-  glEnableVertexAttribArray(0);
+
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(sizeof(float) * 3));
+  glEnableVertexAttribArray(1);
 
   glBindBuffer(GL_ARRAY_BUFFER, 0);
   glBindVertexArray(0);
@@ -121,21 +111,15 @@ int main(const int argc, const char* argv[]) {
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    glUseProgram(shaderProgramOrange);
-    glBindVertexArray(VAOs[0]);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
-
-    glUseProgram(shaderProgramYellow);
-    glBindVertexArray(VAOs[1]);
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);
     glDrawArrays(GL_TRIANGLES, 0, 3);
 
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
-  glDeleteProgram(shaderProgramOrange);
-  glDeleteProgram(shaderProgramYellow);
-  glDeleteBuffers(2, VBOs);
-  glDeleteVertexArrays(2, VAOs);
+  glDeleteBuffers(1, &VBO);
+  glDeleteVertexArrays(1, &VAO);
   glfwTerminate();
   return 0;
 }
